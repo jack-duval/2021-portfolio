18823f8fdc30e9319673248d4e8565bc4704bea0
John (Jack) Duval <42106216+jack-duval@users.noreply.github.com>
2021-12-24 01:30:06 -0500

    overflow text fix

--- src/Biography/Biography.jsx
+++ src/Biography/Biography.jsx
@@ -14,7 +14,7 @@ const Biography = () => {
                 On weekends, you can find me coaching beginners and experienced sabre fencers of all ages at <a href="https://dynamo-fencing.com/">Dynamo Fencing Center</a>.
             </p>
             <p>
-                Previously, I worked at <a href="https://www.edmit.me/">Edmit</a>, an ed-tech startup that helped students and families see college as an investment, rather than a cost. We shared an accurate estimate of what they'd pay each year, what their potential student loans would be, as well as how much they could earn given their major at that school. While at Edmit, I spent time as a software dev, working on the frontend and backend, data analyst, partnership manager, and content creator. It was awesome to try/learn so many new things.
+                Previously, I worked at <a href="https://www.edmit.me/">Edmit</a>, an ed-tech startup that helped students and families see college as an investment, rather than a cost. We shared an accurate estimate of what they'd pay each year, what their potential student loans would be, as well as how much they could earn given their major at that school. While at Edmit, I spent time as a software dev (working on the frontend and backend), data analyst, partnership manager, and content creator. It was awesome to try/learn so many new things.
             </p>
             <p>
                 I've also worked at <a href="https://www.apple.com/">Apple</a> as a retail specialist, <a href="https://www.northeastern.edu/">Northeastern University</a> in IT.
